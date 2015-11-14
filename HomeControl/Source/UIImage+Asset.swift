a868d5c0928aad806c188e0559c860c88c3a861a
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-15 00:54:03 +0100

    Go back to convenience extensions
    This solution is cleaner than `ConvenienceKit`: No need to `import ConvenienceKit` and better autocompletion.

---
+++ HomeControl/Source/UIImage+Asset.swift
@@ -0,0 +1,20 @@
+//
+//  UIImage+Asset.swift
+//  HomeControl
+//
+//  Created by Julian Grosshauser on 15/11/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    enum Asset: String {
+        case Logo = "Logo"
+    }
+
+    convenience init(asset: Asset) {
+        assert(UIImage(named: asset.rawValue) != nil, "No image named \"\(asset.rawValue)\" found in asset catalog")
+        self.init(named: asset.rawValue)!
+    }
+}
