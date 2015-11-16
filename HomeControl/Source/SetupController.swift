1edf93112e677007f28cf4e443a94f6b1abaf622
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-16 02:07:36 +0100

    Add private class TextField

--- HomeControl/Source/SetupController.swift
+++ HomeControl/Source/SetupController.swift
@@ -8,6 +8,31 @@
 
 import UIKit
 
+private class TextField: UITextField {
+    convenience init(placeholder: String, asset: UIImage.Asset) {
+        self.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        widthAnchor.constraintEqualToConstant(300).active = true
+        heightAnchor.constraintEqualToConstant(50).active = true
+        borderStyle = .RoundedRect
+        self.placeholder = placeholder
+        textColor = UIColor(named: .DarkGray)
+
+        let image = UIImage(asset: asset).imageWithRenderingMode(.AlwaysTemplate)
+        let imageView = UIImageView(image: image)
+        imageView.frame = CGRect(x: 0, y: 0, width: imageView.frame.width + 5, height: imageView.frame.height)
+        imageView.contentMode = .Left
+        imageView.tintColor = UIColor(named: .DarkGray)
+        leftView = imageView
+        leftViewMode = .Always
+    }
+
+    override func leftViewRectForBounds(bounds: CGRect) -> CGRect {
+        var rect = super.leftViewRectForBounds(bounds)
+        rect.origin.x += 10
+        return rect
+    }
+}
+
 class SetupController: UIViewController {
 
     //MARK: Properties
