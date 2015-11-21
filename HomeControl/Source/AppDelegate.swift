6eab8bf1e1101b9c6ea43a22bf0ce8381f8308a2
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-21 15:36:48 +0100

    Set theme when app did finish launching

--- HomeControl/Source/AppDelegate.swift
+++ HomeControl/Source/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        ThemeManager.currentTheme = Theme(variant: .Light, primaryColorName: .Red)
         window?.rootViewController = UINavigationController(rootViewController: SetupController())
         window?.makeKeyAndVisible()
         return true
