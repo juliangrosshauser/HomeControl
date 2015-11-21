91cc2bbe315f4aadba559563d11079e245d714a1
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-22 00:05:20 +0100

    Add SetupViewModel class

---
+++ HomeControl/Source/SetupViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SetupViewModel.swift
+//  HomeControl
+//
+//  Created by Julian Grosshauser on 22/11/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import ReactiveCocoa
+
+class SetupViewModel {
+
+    //MARK: Properties
+
+    let serverAddress = MutableProperty("")
+    let username = MutableProperty("")
+    let password = MutableProperty("")
+}
