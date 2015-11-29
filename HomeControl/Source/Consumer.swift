dc211e77266233cd19bf56ea2024aa2ec7c4f5d5
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-29 03:34:05 +0100

    Add Consumer struct

---
+++ HomeControl/Source/Consumer.swift
@@ -0,0 +1,15 @@
+//
+//  Consumer.swift
+//  HomeControl
+//
+//  Created by Julian Grosshauser on 29/11/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+struct Consumer {
+
+    //MARK: Properties
+
+    let name: String
+    let actionID: String
+}
