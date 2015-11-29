0c044e5547644166d1a41727754f41446fb01620
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-29 03:21:30 +0100

    Add Room struct

---
+++ HomeControl/Source/Room.swift
@@ -0,0 +1,15 @@
+//
+//  Room.swift
+//  HomeControl
+//
+//  Created by Julian Grosshauser on 29/11/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+struct Room {
+
+    //MARK: Properties
+
+    let id: UInt
+    let name: String
+}
