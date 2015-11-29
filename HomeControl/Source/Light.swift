c8ec1d615b4803872e9169913122b6731290e91a
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-29 03:25:32 +0100

    Add Light struct

---
+++ HomeControl/Source/Light.swift
@@ -0,0 +1,15 @@
+//
+//  Light.swift
+//  HomeControl
+//
+//  Created by Julian Grosshauser on 29/11/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+struct Light {
+
+    //MARK: Properties
+
+    let name: String
+    let actionID: String
+}
