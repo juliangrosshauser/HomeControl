a0bfb490577caf9de7ac3003c06f84152842e816
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-29 03:30:38 +0100

    Add Blind struct

---
+++ HomeControl/Source/Blind.swift
@@ -0,0 +1,15 @@
+//
+//  Blind.swift
+//  HomeControl
+//
+//  Created by Julian Grosshauser on 29/11/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+struct Blind {
+
+    //MARK: Properties
+
+    let name: String
+    let actionID: String
+}
