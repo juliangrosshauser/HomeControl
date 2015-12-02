3d8335e216848503158b7cd1d927dc5d9e366109
Julian Grosshauser <julian@juliangrosshauser.com>
2015-12-03 00:49:57 +0100

    Parse structure file

--- HomeControl/Source/Store.swift
+++ HomeControl/Source/Store.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2015 Julian Grosshauser. All rights reserved.
 //
 
-class Store {}
+import ReactiveCocoa
+import SWXMLHash
+
+enum StoreError: ErrorType {
+    case NetworkError(ErrorType?)
+    case ReadError(ErrorType)
+}
+
+class Store {
+
+    func parseStructureFile(path: String) -> SignalProducer<[Room], StoreError> {
+        let structureFileContent: String
+
+        do {
+            structureFileContent = try String(contentsOfFile: path, encoding: NSUTF8StringEncoding)
+        } catch {
+            return SignalProducer(error: .ReadError(error))
+        }
+
+        let structureFile = SWXMLHash.parse(structureFileContent)
+        var rooms = [Room]()
+
+        for room in structureFile["LoxLIVE"]["Rooms"]["Room"] {
+            if let idString = room.element?.attributes["n"], id = UInt(idString), name = room.element?.attributes["name"] {
+                rooms.append(Room(id: id, name: name))
+            }
+        }
+
+        return SignalProducer(value: rooms)
+    }
+}
