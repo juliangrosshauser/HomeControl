02ece9d5c06d475bb023c6ee21abaf30ecf90a3e
Julian Grosshauser <julian@juliangrosshauser.com>
2015-12-05 23:59:29 +0100

    Add NetworkManagerSpec class to unit test target

---
+++ HomeControlTests/Source/NetworkManagerSpec.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManagerSpec.swift
+//  HomeControl
+//
+//  Created by Julian Grosshauser on 05/12/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import HomeControl
+
+class NetworkManagerSpec: QuickSpec {}
