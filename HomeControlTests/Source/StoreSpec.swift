48e5501e9d343cc53f18c06a0b6d407da0e041e5
Julian Grosshauser <julian@juliangrosshauser.com>
2015-12-12 23:47:56 +0100

    Add StoreSpec test class

---
+++ HomeControlTests/Source/StoreSpec.swift
@@ -0,0 +1,13 @@
+//
+//  StoreSpec.swift
+//  HomeControl
+//
+//  Created by Julian Grosshauser on 12/12/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import HomeControl
+
+class StoreSpec: QuickSpec {}
