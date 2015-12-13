88aa0b7da465d37482c75609ae5512e7354c21da
Julian Grosshauser <julian@juliangrosshauser.com>
2015-12-13 23:58:41 +0100

    Test parseStructureFile with valid structure file

--- HomeControlTests/Source/StoreSpec.swift
+++ HomeControlTests/Source/StoreSpec.swift
@@ -10,4 +10,45 @@ import Quick
 import Nimble
 @testable import HomeControl
 
-class StoreSpec: QuickSpec {}
+class StoreSpec: QuickSpec {
+
+    override func spec() {
+        describe("Store") {
+            var store: Store!
+            beforeEach { store = Store() }
+
+            describe("parseStructureFile") {
+                let structureFileFolder = NSBundle(forClass: StoreSpec.self).resourceURL!.URLByAppendingPathComponent("StructureFiles")
+
+                context("valid structure file") {
+                    let validStructureFilePath = structureFileFolder.URLByAppendingPathComponent("ValidStructureFile.xml").path!
+
+                    it("returns rooms without error") {
+                        let signalProducer = store.parseStructureFile(validStructureFilePath)
+
+                        var values = [[Room]]()
+                        var error: StoreError?
+                        var completed = false
+
+                        signalProducer.start { event in
+                            switch event {
+                            case let .Next(value):
+                                values.append(value)
+                            case let .Failed(storeError):
+                                error = storeError
+                            case .Completed:
+                                completed = true
+                            default:
+                                break
+                            }
+                        }
+
+                        expect(values.count).to(equal(1))
+                        expect(error).to(beNil())
+                        expect(completed).to(beTrue())
+                    }
+                }
+            }
+        }
+    }
+}
