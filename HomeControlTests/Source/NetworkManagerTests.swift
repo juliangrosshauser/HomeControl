a7014365ed813f15f32ddf234afba6315cc427b0
Julian Grosshauser <julian@juliangrosshauser.com>
2015-12-16 02:18:14 +0100

    Replace QuickSpec classes with XCTestCase classes

---
+++ HomeControlTests/Source/NetworkManagerTests.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManagerTests.swift
+//  HomeControl
+//
+//  Created by Julian Grosshauser on 16/12/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import XCTest
+@testable import HomeControl
+
+class NetworkManagerTests: XCTestCase {}
---
+++ HomeControlTests/Source/StoreTests.swift
@@ -0,0 +1,51 @@
+//
+//  StoreTests.swift
+//  HomeControl
+//
+//  Created by Julian Grosshauser on 16/12/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import XCTest
+@testable import HomeControl
+
+class StoreTests: XCTestCase {
+
+    var store: Store!
+    let structureFileFolder = NSBundle(forClass: StoreTests.self).resourceURL!.URLByAppendingPathComponent("StructureFiles")
+
+    override func setUp() {
+        store = Store()
+    }
+
+    func testParsingValidStructureFileDoesntThrow() {
+        let validStructureFilePath = structureFileFolder.URLByAppendingPathComponent("ValidStructureFile.xml").path!
+        let expectation = expectationWithDescription("Parsing valid structure file doesn't throw")
+
+        store.parseStructureFile(validStructureFilePath) { result in
+            do {
+                try result()
+                expectation.fulfill()
+            } catch {
+                XCTFail("Parsing structure file threw error")
+            }
+        }
+
+        waitForExpectationsWithTimeout(1, handler: nil)
+    }
+
+    func testParsingInvalidStructureFileThrows() {
+        let invalidStructureFilePath = structureFileFolder.URLByAppendingPathComponent("InvalidStructureFile.xml").path!
+        let expectation = expectationWithDescription("Parsing invalid structure file throws")
+
+        store.parseStructureFile(invalidStructureFilePath) { result in
+            do {
+                try result()
+            } catch {
+                expectation.fulfill()
+            }
+        }
+
+        waitForExpectationsWithTimeout(1, handler: nil)
+    }
+}
