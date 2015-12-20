bf2dd650a118cb7be6de703a685610a5342d22cc
Julian Grosshauser <julian@juliangrosshauser.com>
2015-12-20 17:58:07 +0100

    Add documentation to AuthenticationData

--- HomeControl/Source/AuthenticationData.swift
+++ HomeControl/Source/AuthenticationData.swift
@@ -6,11 +6,18 @@
 //  Copyright © 2015 Julian Grosshauser. All rights reserved.
 //
 
+
+/// Contains all data necessary to authenticate a user at the server.
 struct AuthenticationData {
 
     //MARK: Properties
-    
+
+    /// Address of server.
     let serverAddress: String
+
+    /// Name that identifies user.
     let username: String
+
+    /// Password of user.
     let password: String
 }
