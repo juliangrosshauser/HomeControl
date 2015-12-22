6fc375f62961b571a9ce8df60adfc9ab0cc91a57
Julian Grosshauser <julian@juliangrosshauser.com>
2015-12-22 20:59:44 +0100

    Add documentation to Room

--- HomeControl/Source/Room.swift
+++ HomeControl/Source/Room.swift
@@ -6,18 +6,30 @@
 //  Copyright © 2015 Julian Grosshauser. All rights reserved.
 //
 
+
+/// Represents a room containing lights, blinds and consumers.
 struct Room {
 
     //MARK: Properties
 
+    /// Room ID.
     let id: UInt
+    
+    /// Room name.
     let name: String
+    
+    /// Lights.
     var lights: [Light]?
+    
+    /// Blinds.
     var blinds: [Blind]?
+    
+    /// Consumers.
     var consumers: [Consumer]?
 
     //MARK: Initialization
 
+    /// Construct a `Room` without any lights, blinds or consumers.
     init(id: UInt, name: String) {
         self.id = id
         self.name = name
