5d36557d48ad5f703ffc34ec6ccd0ea915a55f5d
Julian Grosshauser <julian@juliangrosshauser.com>
2015-12-22 21:08:54 +0100

    Add Accessory protocol

---
+++ HomeControl/Source/Accessory.swift
@@ -0,0 +1,21 @@
+//
+//  Accessory.swift
+//  HomeControl
+//
+//  Created by Julian Grosshauser on 22/12/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import Foundation
+
+/// Representing an accessory, like lights, blinds and consumers.
+protocol Accessory {
+    
+    //MARK: Properties
+    
+    /// Accessory name
+    var name: String { get }
+    
+    /// Action ID of accessory. Can be used to trigger accessory actions.
+    var actionID: String { get }
+}
