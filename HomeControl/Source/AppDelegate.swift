6a0b86eb3dcde7471ff3090a00237b1d8a27876d
Julian Grosshauser <julian@juliangrosshauser.com>
2015-12-31 22:07:14 +0100

    Add store property to app delegate

--- HomeControl/Source/AppDelegate.swift
+++ HomeControl/Source/AppDelegate.swift
@@ -11,11 +11,17 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    //MARK: Properties
+
     var window: UIWindow? = {
         let window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window.backgroundColor = .whiteColor()
         return window
     }()
+
+    private let store = Store()
+
+    //MARK: UIApplicationDelegate
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         window?.rootViewController = UINavigationController(rootViewController: SetupController())
