ab6f6901751bfe41150f83f18ab54320364661fb
Julian Grosshauser <julian@juliangrosshauser.com>
2015-12-31 22:07:47 +0100

    Add helper methods to app delegate

--- HomeControl/Source/AppDelegate.swift
+++ HomeControl/Source/AppDelegate.swift
@@ -28,4 +28,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         return true
     }
+
+    //MARK: Helpers
+
+    private func fetchRooms() -> [Room] {
+        let request = ManagedRoom.sortedFetchRequest
+        request.returnsObjectsAsFaults = false
+        request.relationshipKeyPathsForPrefetching = ["lights", "blinds", "consumers"]
+
+        do {
+            guard let rooms = try store.context.executeFetchRequest(request) as? [ManagedRoom] else {
+                fatalError("Fetched objects should be of type ManagedRoom")
+            }
+
+            return rooms.map {
+                $0.immutable
+            }
+        } catch {
+            fatalError("Executing fetch request failed")
+        }
+    }
+
+    private func setupRootViewController(rooms: [Room]) -> UIViewController {
+        if rooms.isEmpty {
+            return SetupController(viewModel: SetupViewModel(store: store))
+        }
+
+        // TODO: Set up UI
+        return UIViewController()
+    }
 }
