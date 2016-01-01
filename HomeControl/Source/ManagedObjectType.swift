6f1d91e61731ab25d0ca83a60af7c7339212008c
Julian Grosshauser <julian@juliangrosshauser.com>
2016-01-01 23:25:49 +0100

    Add NSManagedObject extension to ManagedObjectType

--- HomeControl/Source/ManagedObjectType.swift
+++ HomeControl/Source/ManagedObjectType.swift
@@ -28,3 +28,58 @@ extension ManagedObjectType {
         return request
     }
 }
+
+//MARK: NSManagedObject
+
+extension ManagedObjectType where Self: NSManagedObject {
+
+    public static func findOrCreateInContext(context: NSManagedObjectContext, matchingPredicate predicate: NSPredicate, configure: Self -> Void) -> Self {
+        if let object = findOrFetchInContext(context, matchingPredicate: predicate) {
+            return object
+        }
+
+        let object: Self = context.insertObject()
+        configure(object)
+        return object
+    }
+
+
+    public static func findOrFetchInContext(context: NSManagedObjectContext, matchingPredicate predicate: NSPredicate) -> Self? {
+        if let object = materializedObjectInContext(context, matchingPredicate: predicate) {
+            return object
+        }
+
+        return fetchInContext(context) { request in
+            request.predicate = predicate
+            request.returnsObjectsAsFaults = false
+            request.fetchLimit = 1
+        }.first
+    }
+
+    public static func materializedObjectInContext(context: NSManagedObjectContext, matchingPredicate predicate: NSPredicate) -> Self? {
+        for object in context.registeredObjects where !object.fault {
+            guard let result = object as? Self where predicate.evaluateWithObject(result) else {
+                continue
+            }
+
+            return result
+        }
+
+        return nil
+    }
+
+    public static func fetchInContext(context: NSManagedObjectContext, @noescape configure: NSFetchRequest -> Void = { _ in }) -> [Self] {
+        let request = NSFetchRequest(entityName: Self.entityName)
+        configure(request)
+
+        do {
+            guard let result = try context.executeFetchRequest(request) as? [Self] else {
+                fatalError("Fetched objects should be of type \(Self.self)")
+            }
+
+            return result
+        } catch {
+            fatalError("Executing fetch request failed")
+        }
+    }
+}
