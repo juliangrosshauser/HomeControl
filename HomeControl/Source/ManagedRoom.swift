0ffa069ff0651e7bdd27026738769d51f4e8c7a2
Julian Grosshauser <julian@juliangrosshauser.com>
2016-01-02 23:45:59 +0100

    Let ManagedRoom conform to StructConvertible

--- HomeControl/Source/ManagedRoom.swift
+++ HomeControl/Source/ManagedRoom.swift
@@ -15,7 +15,37 @@ public final class ManagedRoom: NSManagedObject {
     @NSManaged public private(set) var id: Int32
     @NSManaged public private(set) var name: String
 
-    public var immutable: Room {
+    //MARK: Relationships
+
+    @NSManaged public private(set) var lights: Set<ManagedLight>?
+    @NSManaged public private(set) var blinds: Set<ManagedBlind>?
+    @NSManaged public private(set) var consumers: Set<ManagedConsumer>?
+}
+
+//MARK: ManagedObjectType
+
+extension ManagedRoom: ManagedObjectType {
+
+    public static var entityName: String {
+        return "Room"
+    }
+
+    public static var defaultSortDescriptors: [NSSortDescriptor] {
+        return [NSSortDescriptor(key: "name", ascending: false)]
+    }
+}
+
+//MARK: StructConvertible
+
+extension ManagedRoom: StructConvertible {
+
+    //MARK: Associated Types
+
+    public typealias StructType = Room
+
+    //MARK: Methods
+
+    public func convertToStruct() -> StructType {
         let immutableLights = lights?.map {
             $0.immutable
         }
@@ -31,50 +61,30 @@ public final class ManagedRoom: NSManagedObject {
         return Room(id: UInt(id), name: name, lights: immutableLights, blinds: immutableBlinds, consumers: immutableConsumers)
     }
 
-    //MARK: Relationships
+    public func configure(item: StructType) {
+        guard let managedObjectContext = managedObjectContext else {
+            fatalError("Managed object needs to be inserted into managed object context before it can be configured")
+        }
 
-    @NSManaged public private(set) var lights: Set<ManagedLight>?
-    @NSManaged public private(set) var blinds: Set<ManagedBlind>?
-    @NSManaged public private(set) var consumers: Set<ManagedConsumer>?
+        id = Int32(item.id)
+        name = item.name
 
-    //MARK: ManagedRoom
-
-    public static func insert(room: Room, intoContext context: NSManagedObjectContext) -> ManagedRoom {
-        let managedRoom: ManagedRoom = context.insertObject()
-        managedRoom.id = Int32(room.id)
-        managedRoom.name = room.name
-
-        if let lights = room.lights {
-            managedRoom.lights = Set(lights.map {
-                ManagedLight.insert($0, intoContext: context)
+        if let lights = item.lights {
+            self.lights = Set(lights.map {
+                ManagedLight.insert($0, intoContext: managedObjectContext)
             })
         }
 
-        if let blinds = room.blinds {
-            managedRoom.blinds = Set(blinds.map {
-                ManagedBlind.insert($0, intoContext: context)
+        if let blinds = item.blinds {
+            self.blinds = Set(blinds.map {
+                ManagedBlind.insert($0, intoContext: managedObjectContext)
             })
         }
 
-        if let consumers = room.consumers {
-            managedRoom.consumers = Set(consumers.map {
-                ManagedConsumer.insert($0, intoContext: context)
+        if let consumers = item.consumers {
+            self.consumers = Set(consumers.map {
+                ManagedConsumer.insert($0, intoContext: managedObjectContext)
             })
         }
-
-        return managedRoom
-    }
-}
-
-//MARK: ManagedObjectType
-
-extension ManagedRoom: ManagedObjectType {
-
-    public static var entityName: String {
-        return "Room"
-    }
-
-    public static var defaultSortDescriptors: [NSSortDescriptor] {
-        return [NSSortDescriptor(key: "name", ascending: false)]
     }
 }
